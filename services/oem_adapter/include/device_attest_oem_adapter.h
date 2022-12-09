729a70e8ddef1f9e71c35a02d87edde97c4b8b90
徐山山 <xushanshan11@huawei.com>
2022-12-09 14:47:18 +0800

    修改device_attest模块进程权限
    Signed-off-by: 徐山山 <xushanshan11@huawei.com> 0dc806a

--- services/oem_adapter/include/device_attest_oem_adapter.h
+++ services/oem_adapter/include/device_attest_oem_adapter.h
@@ -31,7 +31,7 @@ extern "C" {
 #define TOKEN_SIZE 151
 #define TOKEN_FLAG_SIZE 4
 #define TOKEN_WITH_FLAG_SIZE (TOKEN_SIZE + TOKEN_FLAG_SIZE)
-#define TOKEN_ADDR "/data/data"
+#define TOKEN_ADDR "/data/device_attest"
 #define TOKEN_A_ADDR "tokenA"
 #define TOKEN_B_ADDR "tokenB"
 #define BITS_PER_BYTE 8

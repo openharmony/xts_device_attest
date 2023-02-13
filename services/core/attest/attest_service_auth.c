2558d05c9c98db1437d4855a7bc08eaa07769a48
徐山山 <xushanshan11@huawei.com>
2023-02-13 20:16:06 +0800

    认证后立即存储认证结果
    Signed-off-by: 徐山山 <xushanshan11@huawei.com> 0dc806a

--- services/core/attest/attest_service_auth.c
+++ services/core/attest/attest_service_auth.c
@@ -69,7 +69,8 @@ bool IsAuthStatusChg(void)
 
 int32_t FlushAuthResult(const char* ticket, const char* authStatus)
 {
-    if (ticket == NULL || WriteTicketToDevice(ticket, strlen(ticket)) != 0) {
+    // ticket is not always need to write, and we don't care about writing it succeed or not.
+    if (ticket != NULL && WriteTicketToDevice(ticket, strlen(ticket)) != 0) {
         ATTEST_LOG_WARN("[FlushAuthResult] write ticket failed");
     }
 
@@ -321,11 +322,10 @@ int32_t CheckAuthResult(AuthStatus* authStatus, uint64_t currentTime)
         return ATTEST_ERR;
     }
 
-    int32_t result = authStatus->hardwareResult;
     uint64_t expireTime = authStatus->expireTime;
 
-    if (result != ATTEST_OK || expireTime <= currentTime) {
-        ATTEST_LOG_ERROR("[CheckAuthResult] result or expireTime is Wrong, result = %d.", result);
+    if (expireTime <= currentTime) {
+        ATTEST_LOG_ERROR("[CheckAuthResult] expireTime is Wrong.");
         return ATTEST_ERR;
     }
 
@@ -756,6 +756,7 @@ int32_t ParseAuthResultResp(const char* msg, AuthResult* authResult)
         }
         if (ParseTokenValue(json, authResult) != 0) {
             ATTEST_LOG_ERROR("[ParseAuthResultResp] Parse token value from symbol authentication response failed");
+            ret = 0;
             break;
         }
         if (ParseTokenId(json, authResult) != 0) {

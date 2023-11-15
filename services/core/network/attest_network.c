c39346716cb5b20bd5a5732bd070f2682e709a78
keminLuo <luojianming4@h-partners.com>
2023-11-15 16:24:08 +0800

    软本版本号采用用户可见版本号，不带openharmony字样；解决可能会crash的地方；解决外部依赖失败 Signed-off-by: 罗键铭 <541416002@qq.com>

--- services/core/network/attest_network.c
+++ services/core/network/attest_network.c
@@ -365,7 +365,6 @@ static int32_t InitSocketClient(int32_t *socketFd)
 
 static int32_t InitSSLSocket(int32_t socketFd, SSL **socketSSL)
 {
-    int32_t retCode;
     int32_t ret = ATTEST_ERR;
     char *caFile = "/etc/ssl/certs/cacert.pem";
     SSL_library_init();
@@ -380,7 +379,7 @@ static int32_t InitSSLSocket(int32_t socketFd, SSL **socketSSL)
     do {
         /* 设置根证书检验 */
         SSL_CTX_set_verify(socketCTX, SSL_VERIFY_PEER, NULL);
-        retCode = SSL_CTX_load_verify_locations(socketCTX, caFile, NULL);
+        int32_t retCode = SSL_CTX_load_verify_locations(socketCTX, caFile, NULL);
         if (retCode != SSL_OK) {
             ATTEST_LOG_ERROR("[InitSSLSocket] InitSSL load_verify fail \n");
             ret = ATTEST_ERR;
